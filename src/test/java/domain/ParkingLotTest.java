831f57483382b9f57e25ec240d707fb8b6f47dc7
mike.yan <yancy3@lenovo.com>
2019-06-17 13:28:35 +0800

    [feat] the basic scenario: park a car into a parking-lot

--- src/test/java/domain/ParkingLotTest.java
+++ src/test/java/domain/ParkingLotTest.java
@@ -16,105 +16,83 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class ParkingLotTest {
     @Test
-    void should_succeed_when_parking_given_a_car_and_a_not_full_parking_lot() throws NoCarToParkException, FullyOccupiedParkingLotException {
-        //given
+    void should_succeed_and_get_a_ticket_when_parking_given_a_car_and_a_not_full_parking_lot() throws NoCarToParkException, FullyOccupiedParkingLotException {
         ParkingLot parkingLot = getEmptyParkingLot(10);
         Car car = new Car();
 
-        //when
         Tickets ticket = parkingLot.park(car);
 
-        //then
         assertNotNull(ticket);
-        assertEquals(1, parkingLot.occupiedSlotNumbers());
+        assertEquals(1, parkingLot.occupiedSlotsNumber());
     }
 
     @Test
     void should_fail_when_parking_given_a_car_and_a_full_parking_lot() {
-        //given
         ParkingLot parkingLot = getFullParkingLot(10);
         Car car = new Car();
 
-        //when
         assertThrows(FullyOccupiedParkingLotException.class, () -> parkingLot.park(car));
 
-        //then
-        assertEquals(10, parkingLot.occupiedSlotNumbers());
+        assertEquals(10, parkingLot.occupiedSlotsNumber());
     }
 
     @Test
-    void should_succeed_when_picking_up_given_a_parked_car_and_a_valid_ticket() throws FullyOccupiedParkingLotException, NoCarToParkException, CarPickingUpWithNoTicketException, InvalidTicketException {
-        //given
+    void should_succeed_and_get_the_car_when_picking_up_given_a_parked_car_and_the_referenced_valid_ticket() throws FullyOccupiedParkingLotException, NoCarToParkException, CarPickingUpWithNoTicketException, InvalidTicketException {
         ParkingLot parkingLot = getEmptyParkingLot(10);
         Car carToPark = new Car();
         Tickets ticket = parkingLot.park(carToPark);
 
-        //when
         Car carPickedUp = parkingLot.pickUpCar(ticket);
 
-        //then
         assertNotNull(carPickedUp);
-        assertEquals(0, parkingLot.occupiedSlotNumbers());
+        assertEquals(0, parkingLot.occupiedSlotsNumber());
     }
-
 
     @Test
     void should_fail_when_picking_up_given_a_parked_car_and_no_ticket() throws FullyOccupiedParkingLotException, NoCarToParkException {
-        //given
         ParkingLot parkingLot = getEmptyParkingLot(10);
         Car carToPark = new Car();
         parkingLot.park(carToPark);
 
-        //when
         assertThrows(CarPickingUpWithNoTicketException.class, () -> parkingLot.pickUpCar(null));
 
-        //then
-        assertEquals(1, parkingLot.occupiedSlotNumbers());
+        assertEquals(1, parkingLot.occupiedSlotsNumber());
     }
 
     @Test
-    void should_succeed_when_picking_up_given_a_parked_car_and_the_related_valid_ticket() throws NoCarToParkException, FullyOccupiedParkingLotException, CarPickingUpWithNoTicketException, InvalidTicketException {
-        //given
+    void should_succeed_and_get_the_car_when_picking_up_given_a_parked_car_and_the_related_valid_ticket() throws NoCarToParkException, FullyOccupiedParkingLotException, CarPickingUpWithNoTicketException, InvalidTicketException {
         ParkingLot parkingLot = getEmptyParkingLot(10);
         Car carToPark = new Car();
         Tickets ticket = parkingLot.park(carToPark);
 
-        //when
         Car carPickedUp = parkingLot.pickUpCar(ticket);
 
-        //then
         assertEquals(carToPark, carPickedUp);
-        assertEquals(0, parkingLot.occupiedSlotNumbers());
+        assertEquals(0, parkingLot.occupiedSlotsNumber());
     }
 
     @Test
     void should_fail_when_picking_up_given_a_parked_car_and_an_invalid_ticket() throws NoCarToParkException, FullyOccupiedParkingLotException {
-        //given
         ParkingLot parkingLot = getEmptyParkingLot(10);
         Car carToPark = new Car();
         parkingLot.park(carToPark);
         Tickets ticket = Tickets.getTicket();
 
-        //when
         assertThrows(InvalidTicketException.class, () -> parkingLot.pickUpCar(ticket));
 
-        //then
-        assertEquals(1, parkingLot.occupiedSlotNumbers());
+        assertEquals(1, parkingLot.occupiedSlotsNumber());
     }
 
     @Test
     void should_fail_when_picking_up_given_a_parked_car_and_an_used_ticket() throws NoCarToParkException, FullyOccupiedParkingLotException {
-        //given
         ParkingLot parkingLot = getEmptyParkingLot(10);
         Car carToPark = new Car();
         Tickets ticket = parkingLot.park(carToPark);
         Tickets.destroyTicket(ticket);
 
-        //when
         assertThrows(InvalidTicketException.class, () -> parkingLot.pickUpCar(ticket));
 
-        //then
-        assertEquals(1, parkingLot.occupiedSlotNumbers());
+        assertEquals(1, parkingLot.occupiedSlotsNumber());
     }
 
 }
